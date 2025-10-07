296c7c921b9f86be4e81a0d3e7f77f0bf9061a71
Rudrapratap Singh chauhan <rudrapratapscc090907@gmail.com>
2025-10-07 11:09:59 +0530

    demo2

---
+++ batch_2/rudra7oct1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+        int a=100;
+        while (a>=1){
+                printf("\n%d",a);
+                a=a-3;
+        }
+        return 0;
+}
+~                                                                                                                               
+~                     
